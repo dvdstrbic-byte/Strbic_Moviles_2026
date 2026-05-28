a41c06ee6ec770d1271985f47143ee8948fff408
daviddd <dvdstrbic@gmail.com>
2026-05-27 22:01:40 -0300

    Update perfil.tsx

--- perfil.tsx
+++ perfil.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function Perfil() {
   return (
         <View>
-      <Text>Nombre: Federico García</Text>
+      <Text>Nombre: David Strbic</Text>
       <Text>Edad: 18 años</Text>
       <Text>Curso: 7mo 2da Programación</Text>
     </View>
